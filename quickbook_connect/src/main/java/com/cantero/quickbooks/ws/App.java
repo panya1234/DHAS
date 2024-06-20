fc86255e081561eb9f479ff79b3e11e6283e4d3a
Panya <Panyakan@ignite-idea.com>
2024-06-20 14:55:40 +0700

    jarrrr

--- quickbook_connect/src/main/java/com/cantero/quickbooks/ws/App.java
+++ quickbook_connect/src/main/java/com/cantero/quickbooks/ws/App.java
@@ -20,23 +20,23 @@ public class App {
         // System.out.print("Test CustomerAddRqSoapImpl");
 
         System.out.println("--Indonesia--");
-        Endpoint.publish("http://dev-apiqbook.dhas.com/AddSOIndonesia", new AddSOIndonesia());
+        Endpoint.publish("http://0.0.0.0:8080/AddSOIndonesia", new AddSOIndonesia());
         System.out.println("AddSOIndonesia ");
 
-        Endpoint.publish("http://dev-apiqbook.dhas.com/GetINItemInventory", new GetINItemInventory());
+        Endpoint.publish("http://0.0.0.0:8080/GetINItemInventory", new GetINItemInventory());
         System.out.println("GetINItemInventory ");
 
-        Endpoint.publish("http://dev-apiqbook.dhas.com/GetINCustomer", new GetINCustomer());
+        Endpoint.publish("http://0.0.0.0:8080/GetINCustomer", new GetINCustomer());
         System.out.println("GetINCustomer ");
 
         System.out.println("--Philippines--");
-        Endpoint.publish("http://dev-apiqbook.dhas.com/AddSOPhilippines", new AddSOPhilippines());
+        Endpoint.publish("http://0.0.0.0:8080/AddSOPhilippines", new AddSOPhilippines());
         System.out.println("AddSOPhilippines ");
 
-        Endpoint.publish("http://dev-apiqbook.dhas.com/GetPHPItemInventory", new GetPHPItemInventory());
+        Endpoint.publish("http://0.0.0.0:8080/GetPHPItemInventory", new GetPHPItemInventory());
         System.out.println("GetPHPItemInventory ");
 
-        Endpoint.publish("http://dev-apiqbook.dhas.com/GetPHPCustomer", new GetPHPCustomer());
+        Endpoint.publish("http://0.0.0.0:8080/GetPHPCustomer", new GetPHPCustomer());
         System.out.println("GetPHPCustomer ");
     }
 } // or the version compatible with your JDK
