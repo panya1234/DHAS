071125964eb35667feb639ff7dc3907dda1f1d24
Panya <Panyakan@ignite-idea.com>
2024-08-09 15:44:42 +0700

    update

--- quickbook_connect/src/main/java/com/cantero/quickbooks/ws/GetPHPSequence.java
+++ quickbook_connect/src/main/java/com/cantero/quickbooks/ws/GetPHPSequence.java
@@ -68,7 +68,7 @@ public class GetPHPSequence implements QBWebConnectorSvcSoap {
         // TODO Auto-generated method stub
         // System.out.println(response);
 
-        final String FILE_CANCAL_PATH = "CSV/WRITE/CancelOrders/Philippines/cancelOrdersPHP.csv";
+        final String FILE_CANCAL_PATH = "CSV/WRITE/CancelOrders/Philippines/cancelOrdersPH.csv";
         try (CSVReader readerCancelSO = new CSVReader(new FileReader(FILE_CANCAL_PATH))){
             List<String[]> allRows = readerCancelSO.readAll();
             readerCancelSO.skip(1);
