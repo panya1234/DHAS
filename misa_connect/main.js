5ff4923797789aec24c6a4d55f43bfcbc9c90944
Panya <Panyakan@ignite-idea.com>
2024-09-11 14:45:09 +0700

    sd

--- misa_connect/main.js
+++ misa_connect/main.js
@@ -12,19 +12,7 @@ import { mainPostVNSProducts } from './misaPostVNSProducts.js';
 import { mainPostVNNOrders } from './misaPostVNNOrders.js';
 import { mainPostVNSOrders } from './misaPostVNSOrders.js';
 
-import { mainGetToken } from './misaGetToken.js';
-
 import { JOB_SCHEDULE_VNNACCOUNTS, JOB_SCHEDULE_VNNPRODUCTS, JOB_SCHEDULE_VNSPRODUCTS, JOB_SCHEDULE_ORDERS, JOB_SCHEDULE_TOKEN } from './misaConfig.js';
-
-const taskToken = cron.schedule(JOB_SCHEDULE_TOKEN, async () => {
-    try {
-        await mainGetToken()
-        console.log('1. mainPostOrders Success!'+ Date.now());
-    } catch (error) {
-        console.error('Error posting accounts:', error);
-        taskToken.stop();
-    }
-});
 
 const taskOrders = cron.schedule(JOB_SCHEDULE_ORDERS, async () => {
     try {
@@ -73,8 +61,6 @@ const taskVNSProducts = cron.schedule(JOB_SCHEDULE_VNSPRODUCTS, async () => {
 const app = express();
 app.listen(8800, () => {
     console.log('Server is running on port 8800');
-    mainGetToken();
-    taskToken.start();
     taskOrders.start();
     taskVNNAccounts.start();
     taskVNNProducts.start();
